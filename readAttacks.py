6330a40288b05735d7da383f0699f3852a32016b
Pranav Badhe <44113251+pbadhe@users.noreply.github.com>
2023-03-09 01:28:03 -0500

    Selenium scraping cyberpeaceinstitute data
    ITArmyOfUkraine

---
+++ readAttacks.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+import pandas as pd
+# opt = Options()
+# opt.add_experimental_option("debuggerAddress","localhost:9222")
+# driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe",chrome_options=opt)
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+# driver = webdriver.Chrome(PATH)
+link = "https://cyberconflicts.cyberpeaceinstitute.org/threats/attack-details"
+driver = ""
+
+def launchBrowser():
+	chrome_options = Options()
+	# chrome_options.binary_location=PATH
+	chrome_options.add_argument("disable-infobars");
+	driver = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)
+
+	driver.get(link)
+	time.sleep(5)
+	# driver.find_element(By.CLASS_NAME,"form-input.w-full.text-dark").click()
+	elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
+	print (elements[0].get_attribute('placeholder'))
+	driver.execute_script("arguments[0].value = 'IT Army of Ukraine';", elements[0])
+	time.sleep(4)
+	elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
+	print (elements[0].get_attribute('value'))
+	
+	# time.sleep(5)
+	table_trs = driver.find_elements(By.XPATH, '//table[@class="my-0 w-full"]')
+	print(table_trs[0])
+	df=0
+	if table_trs is not None:
+		df = pd.read_html(table_trs[0].get_attribute('outerHTML'))
+		print([k for k in df])
+	
+	df.to_csv("Attacks.csv")
+	print("Click done")
+
+	while(True):
+		pass	
+launchBrowser()
+
