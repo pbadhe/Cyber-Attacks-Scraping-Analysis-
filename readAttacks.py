00f02dd50f077dfbccd5b76488c61779e67725a7
Pranav Badhe <44113251+pbadhe@users.noreply.github.com>
2023-03-21 00:45:47 -0400

    Element focus issue

--- readAttacks.py
+++ readAttacks.py
@@ -30,22 +30,25 @@ def launchBrowser():
 	driver = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)
 
 	driver.get(link)
-	time.sleep(5)
 	# driver.find_element(By.CLASS_NAME,"form-input.w-full.text-dark").click()
 	elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
-	print (elements[0].get_attribute('placeholder'))
-	driver.execute_script("arguments[0].value = 'IT Army of Ukraine';", elements[0])
+	elements[0].click()
+	elements[0].send_keys("IT Army of Ukraine",)
+	print (elements[0].get_attribute('placeholder'), "Type of elements[0]: ", type(elements[0]))
+	# driver.execute_script("arguments[0].value = 'IT Army of Ukraine';", elements[0])
+
 	time.sleep(4)
-	elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
-	print (elements[0].get_attribute('value'))
+	# elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
 	
 	# time.sleep(5)
 	table_trs = driver.find_elements(By.XPATH, '//table[@class="my-0 w-full"]')
 	print(table_trs[0])
 	df=0
 	if table_trs is not None:
+		print(table_trs[0].get_attribute('outerHTML'))
 		df = pd.read_html(table_trs[0].get_attribute('outerHTML'))
-		print([k for k in df])
+		for k in df:
+			print(k) 
 	
 	df.to_csv("Attacks.csv")
 	print("Click done")
