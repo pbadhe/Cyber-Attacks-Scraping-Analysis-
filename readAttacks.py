a4e643146df40dd0d65b8d376d1fa90056016c21
pbadhe <pbadhe@iu.edu>
2023-03-22 03:18:34 -0400

    Scraping ALL possible

--- readAttacks.py
+++ readAttacks.py
@@ -2,58 +2,94 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver import Keys, ActionChains
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 import pandas as pd
-# opt = Options()
-# opt.add_experimental_option("debuggerAddress","localhost:9222")
-# driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe",chrome_options=opt)
+from bs4 import BeautifulSoup as bs
+import re, os, time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-# driver = webdriver.Chrome(PATH)
 link = "https://cyberconflicts.cyberpeaceinstitute.org/threats/attack-details"
 driver = ""
 
-def launchBrowser():
+def getCountry(tdCountry):
+	countryPattern = r'data-original-title="([\w\s]+)"'
+	matches = re.search(countryPattern, tdCountry)
+	return (matches.group(1) if matches else "No match found")
+
+def getCertainty(tdCertainty):
+	certaintyPattern = r'data-original-title="([^"]+)"'
+	matches = re.search(certaintyPattern, tdCertainty)
+	return (matches.group(1) if matches else "No match found")
+
+def saveDFToStorage(df, searchFor):
+	if not  os.path.exists("output"):
+		# Create a new directory
+		os.makedirs("output")
+	output_path="output/Attacks_{}.csv".format(searchFor)
+	#Append if exists
+	df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
+	print("\nSave Successful!\n")
+
+def launchBrowser(noOfPages, searchFor):
 	chrome_options = Options()
-	# chrome_options.binary_location=PATH
 	chrome_options.add_argument("disable-infobars");
-	driver = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)
-
+	driver = webdriver.Chrome(executable_path=PATH, chrome_options=chrome_options)	
 	driver.get(link)
-	# driver.find_element(By.CLASS_NAME,"form-input.w-full.text-dark").click()
-	elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
-	elements[0].click()
-	elements[0].send_keys("IT Army of Ukraine",)
-	print (elements[0].get_attribute('placeholder'), "Type of elements[0]: ", type(elements[0]))
-	# driver.execute_script("arguments[0].value = 'IT Army of Ukraine';", elements[0])
 
-	time.sleep(4)
+	#Comment below three lines to search for the whole website
 	# elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@class='form-input w-full text-dark']")))
-	
-	# time.sleep(5)
-	table_trs = driver.find_elements(By.XPATH, '//table[@class="my-0 w-full"]')
-	print(table_trs[0])
-	df=0
-	if table_trs is not None:
-		print(table_trs[0].get_attribute('outerHTML'))
-		df = pd.read_html(table_trs[0].get_attribute('outerHTML'))
-		for k in df:
-			print(k) 
-	
-	df.to_csv("Attacks.csv")
-	print("Click done")
+	# elements[0].click()
+	# elements[0].send_keys(searchFor,)
+
+	time.sleep(2)
+
+	while noOfPages != 0:
+		# time.sleep(6)
+
+		df=0
+		table_trs = driver.find_elements(By.XPATH, '//table[@class="my-0 w-full"]')
+
+		if table_trs is not None:
+			#Select the first table if multiple are found
+			tableOuterHTML = table_trs[0].get_attribute('outerHTML')
+
+			df = pd.read_html(tableOuterHTML)[0]
+			
+			table = bs(tableOuterHTML, "html.parser")
+			countryNames = []
+			certainty = []
+
+			rows = table.find_all("tr")
+			# Leaving Header, slice[1:]
+			for row in rows[1:]:
+				trs = row.find_all("td")
+				print("Country Code: ", getCountry(str(trs[1])))
+				print("TR: ", trs[1])
+				countryNames.append(getCountry(str(trs[1])))
+				certainty.append(getCertainty(str(trs[7])))
+
+			df['Country'] = countryNames
+			df['Certainty'] = certainty
+
+			saveDFToStorage(df, searchFor)
+
+		#Click for the next page
+		element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".svg-inline--fa.fa-angle-right")))
+		element.click()
+		noOfPages-=1
+		time.sleep(0.3)
+
+	print("Done!!")
 
 	while(True):
 		pass	
-launchBrowser()
+
+
+noOfPages = 133 #Attention!
+searchFor = "ALL"
+launchBrowser(noOfPages, searchFor)
 
